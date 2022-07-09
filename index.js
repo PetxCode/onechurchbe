6b7e4f75c4231300fb53083835eb8c7e46d577e9
PetxCode <71887594+PetxCode@users.noreply.github.com>
2022-07-09 08:56:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.status(200).json({ message: "You are welcome to One Church Network" });
+	res.status(200).json({ message: "You are welcome to One Church Network BackEnd API" });
 });
 
 app.use("/api/admin", require("./router/adminRouter"));
